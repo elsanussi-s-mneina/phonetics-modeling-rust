fc778e4e51c3d99554faa74f7069739ca895606e
elsanussi-s-mneina <52800671+elsanussi-s-mneina@users.noreply.github.com>
2020-03-09 23:53:44 -0400

    Branch on user input.

--- phon-mod/src/main.rs
+++ phon-mod/src/main.rs
@@ -11,7 +11,42 @@ fn main()
     io::stdin().read_line(&mut selection)
         .expect("Failed to read user input.");
     
+    selection = selection.trim().to_string();
+    
     println!("The user selected: {}\n", selection);
+    
+    
+    if selection == "1"
+    {
+        view_english_phoneme_inventory();
+    }
+    else if selection == "2"
+    {
+        prompt_for_phoneme_to_voice();
+    }
+    else if selection == "3"
+    {
+        prompt_for_phoneme_to_unvoice()
+    }
+    else
+    {
+       println!("User selection not handled");
+    }
+}
+
+fn view_english_phoneme_inventory()
+{
+    println!("View English phoneme inventory not implemented!");
+}
+
+fn prompt_for_phoneme_to_voice()
+{
+    println!("Prompt for phoneme to voice not implemented!");
+}
+
+fn prompt_for_phoneme_to_unvoice()
+{
+    println!("Prompt for phoneme to unvoice not implemented!");
 }
 
 static MENU: &str = "What do you want to accomplish?
