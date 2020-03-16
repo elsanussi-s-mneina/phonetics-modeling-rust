e367892b68b2f295ae50a1ba1e43e250d5b36839
elsanussi-s-mneina <52800671+elsanussi-s-mneina@users.noreply.github.com>
2020-03-16 15:51:52 -0400

    connect modules
    call currently implemented function in IPA module from main module

--- phon-mod/src/main.rs
+++ phon-mod/src/main.rs
@@ -1,5 +1,6 @@
 mod grapheme;
 
+use crate::grapheme::international_phonetic_alphabet::international_phonetic_alphabet::construct_IPA;
 
 use std::io;
 
@@ -9,6 +10,8 @@ use phon_mod::lib::Place::*;
 use phon_mod::lib::Manner::*;
 use phon_mod::lib::Airstream::*;
 use phon_mod::lib::voiced_phonet;
+use phon_mod::lib::devoiced_phonet;
+
 
 fn main()
 {
@@ -66,8 +69,8 @@ fn prompt_for_phoneme_to_voice()
 
     println!("phoneme {} to voice not implemented!", phoneme);
 
-    let b_phoneme = Consonant {vocal_folds: Voiced, place: Bilabial, manner: Plosive, airstream: PulmonicEgressive}; 
-    println!("Here is a demo: [b] devoiced is {:?}", voiced_phonet(b_phoneme));
+    let p_phoneme = Consonant {vocal_folds: Voiceless, place: Bilabial, manner: Plosive, airstream: PulmonicEgressive};
+    println!("Here is a demo: [p] voiced is {:?}", construct_IPA(voiced_phonet(p_phoneme)));
 }
 
 fn prompt_for_phoneme_to_devoice()
@@ -83,6 +86,9 @@ fn prompt_for_phoneme_to_devoice()
     
 
     println!("phoneme {} to devoice not implemented!", phoneme);
+
+    let b_phoneme = Consonant {vocal_folds: Voiced, place: Bilabial, manner: Plosive, airstream: PulmonicEgressive};
+    println!("Here is a demo: [b] devoiced is {:?}", construct_IPA(devoiced_phonet(b_phoneme)));
 }
 
 static MENU: &str = "What do you want to accomplish?
